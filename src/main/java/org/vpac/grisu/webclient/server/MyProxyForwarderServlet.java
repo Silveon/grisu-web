e351f115a7cb41a2818188b056feb3702415f209
Daniel Davidson <d_avidson@yahoo.co.nz>
2011-02-10 22:35:46 -0800

    Minor changes

--- src/main/java/org/vpac/grisu/webclient/server/MyProxyForwarderServlet.java
+++ src/main/java/org/vpac/grisu/webclient/server/MyProxyForwarderServlet.java
@@ -2,6 +2,7 @@ package org.vpac.grisu.webclient.server;
 
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -10,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.vpac.grisu.webclient.server.serverExceptions.MyProxyCertRequestException;
 
 
 /**
@@ -20,7 +24,7 @@ public class MyProxyForwarderServlet extends HttpServlet {
  private static final long serialVersionUID = 1L;
 public static final String USER_NAME = "userName";
 public static final String USER_PASS = "userPass";
-       
+static final Logger myLogger = Logger.getLogger(GrisuClientServiceImpl.class.getName());
 
  /**
   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -33,6 +37,11 @@ public static final String USER_PASS = "userPass";
 	 
 	 	String userName = "dannyd"; //request.getParameter("userName");
 	 	String userPass = "jfk204$";//request.getParameter("userPass");
+	 	
+	 	myLogger.debug("Remote User = " + request.getRemoteUser());
+	 	myLogger.debug("Authentication TyPE = " + request.getAuthType());
+	 	myLogger.debug("Remote User = " );
+	 	myLogger.debug(request.getAttribute("Shib-Identity-Provider"));
 	 	
 	 	HttpSession session = request.getSession();
 	 	session.setAttribute(USER_NAME, userName);
@@ -56,6 +65,7 @@ public static final String USER_PASS = "userPass";
 	 	String userPass = "jfk204$";//request.getParameter("userPass");
 	 	
 	 	HttpSession session = request.getSession();
+	 	
 	 	session.setAttribute(USER_NAME, userName);
 	 	session.setAttribute(USER_PASS, userPass);
 	 	
@@ -67,5 +77,21 @@ public static final String USER_PASS = "userPass";
 	     dispatcher.forward(request,response);
 	 	
  }
+ 
+ private String getMyProxyCert() throws MyProxyCertRequestException
+ {
+	 
+	 
+	 
+	 
+	 
+	 
+	 return null;
+ }
+ 
+// private HttpSession getSession(HttpServletRequest request)
+// {
+//	return request.getSession(true);
+// }
 
 }
